11906710c0498f7e143aae2157bfc90b9aaf2a49
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2022-11-28 19:34:19 +0200

    create data array

--- src/pages/MarketSelection.jsx
+++ src/pages/MarketSelection.jsx
@@ -3,11 +3,48 @@ import MarketAccordion from "../components/MarketAccordion";
 import "../css/marketSelection.css";
 
 export default function MarketSelection() {
+  let data = [
+    {
+      title: "MONEYLINE",
+      itemsArray: [
+        { bet: "Team 1", value: "+150" },
+        { bet: "Draw", value: "+200" },
+        { bet: "Team 2", value: "+235" },
+      ],
+      itemsInRow: 2,
+    },
+    {
+      title: "BOTH TEAMS TO SCORE",
+      itemsArray: [
+        { bet: "Yes", value: "-110" },
+        { bet: "No", value: "-120" },
+      ],
+      itemsInRow: 3,
+    },
+    {
+      title: "ANY TIME GOALSCORER",
+      itemsArray: [
+        { bet: "Player 1", value: "+115" },
+        { bet: "Player 2", value: "+200" },
+        { bet: "Player 3", value: "+210" },
+        { bet: "Player 4", value: "+250" },
+        { bet: "Player 5", value: "+250" },
+        { bet: "Player 6", value: "+300" },
+        { bet: "Player 7", value: "+360" },
+        { bet: "Player 8", value: "+500" },
+        { bet: "Player 9", value: "+750" },
+        { bet: "Player 10", value: "+780" },
+        { bet: "Player 11", value: "+1000" },
+        { bet: "Player 12", value: "+1100" },
+      ],
+      itemsInRow: 3,
+    },
+  ];
   return (
     <div className="market-selection">
-      <MarketAccordion title="MONEYLINE" itemsInRow={2} />
-      <MarketAccordion title="BOTH TEAMS TO SCORE" />
-      <MarketAccordion title="ANY TIME GOALSCORER" />
+      {data.map((accordion) => (
+        <MarketAccordion />
+      ))}
     </div>
   );
 }
