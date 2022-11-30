3ecf868e251fe630790e41220b7f09bb6d741f22
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2022-11-30 23:25:45 +0200

    add delete button to each single

--- src/components/Betslip/Single.jsx
+++ src/components/Betslip/Single.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import InputFiled from "./InputFiled";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import { removeBet } from "../../redux/features/betsSlice";
 export default function Single({ bet }) {
   const { team1, team2 } = useSelector((store) => store.betsSlice);
+  const dispatch = useDispatch();
   return (
-    <div className="single">
-      <h1>
-        {bet.bet} - {bet.title}
-      </h1>
-      <h2>
-        {team1} vs {team2}
-      </h2>
-      <InputFiled />
-      <div className="orange-square">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur?
+    <div className="single-container">
+      <div className="single">
+        <h1>
+          {bet.bet} - {bet.title}
+        </h1>
+        <h2>
+          {team1} vs {team2}
+        </h2>
+        <InputFiled />
+        <div className="orange-square">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur?
+        </div>
       </div>
+      <RiDeleteBin6Fill
+        onClick={() => dispatch(removeBet(bet))}
+        size={30}
+        index
+        className="delete-icon"
+      />
     </div>
   );
 }
