19dcdc3006f6d77cd71c21b38a200c57ba398142
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2022-11-30 14:26:26 +0200

    add slipCounter style

--- src/components/Betslip/BetSlip.jsx
+++ src/components/Betslip/BetSlip.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "../../css/betSlip.css";
+import SlipCounter from "./SlipCounter";
 export default function BetSlip() {
-  return <div className="betslip">BetSlip</div>;
+  return (
+    <div className="betslip">
+      <SlipCounter />
+    </div>
+  );
 }
---
+++ src/components/Betslip/SlipCounter.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SlipCounter() {
+  return <span className="counter">0</span>;
+}
