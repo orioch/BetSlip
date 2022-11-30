2e26a068905b24bb4bf56238a4bf8305722dd6df
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2022-11-30 20:35:25 +0200

    removeBet when click on image

--- src/components/Betslip/PackageBetsList.jsx
+++ src/components/Betslip/PackageBetsList.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import step from "../../media/step.svg";
 import line from "../../media/Line.svg";
+import { useDispatch } from "react-redux";
+import { removeBet } from "../../redux/features/betsSlice";
 
 export default function PackageBetsList({ array }) {
+  const dispatch = useDispatch();
   return array.map((bet, index) => {
     if (index == array.length - 1)
       return (
         <div className="content-row">
-          <img className="step-img" src={step} />
+          <img
+            onClick={() => dispatch(removeBet(bet))}
+            className="step-img"
+            src={step}
+          />
           <span className="bet-title">{bet.title}</span>
           <span className="bet-selection">{bet.bet}</span>
         </div>
@@ -16,7 +23,11 @@ export default function PackageBetsList({ array }) {
       <React.Fragment>
         {" "}
         <div className="content-row">
-          <img className="step-img" src={step} />
+          <img
+            onClick={() => dispatch(removeBet(bet))}
+            className="step-img"
+            src={step}
+          />
           <span className="bet-title">{bet.title}</span>
           <span className="bet-selection">{bet.bet}</span>
         </div>
