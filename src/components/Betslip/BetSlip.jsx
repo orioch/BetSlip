72d0316bf3b5c388940d31d47bf6a2ff326601c0
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2022-11-30 14:55:32 +0200

    style the value counter component

--- src/components/Betslip/BetSlip.jsx
+++ src/components/Betslip/BetSlip.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../css/betSlip.css";
 import SlipCounter from "./SlipCounter";
+import ValueCounter from "./ValueCounter";
 export default function BetSlip() {
   return (
     <div className="betslip">
       <SlipCounter />
       <h1>Bet Slip</h1>
+      <ValueCounter />
     </div>
   );
 }
---
+++ src/components/Betslip/ValueCounter.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ValueCounter() {
+  return <div className="counter value">+200</div>;
+}
