cda127743e27d5c6e54330a134748c19c6ab5ff5
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2022-12-01 11:49:12 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import BetslipPage from "./pages/BetslipPage";
 import { useSelector, useDispatch } from "react-redux";
 import { openCloseBetslip } from "./redux/features/betsSlice";
 import header from "./media/Header.svg";
+import footer from "./media/footer.svg";
 function App() {
   const dispatch = useDispatch();
   const { isWindowOpen } = useSelector((store) => store.betsSlice);
   return (
     <React.Fragment>
-      <img style={{ width: "100vw" }} src={header} />
+      <img className="header" src={header} />
       <MarketSelection />
       <BetSlipBar />
       <div
         onClick={() => dispatch(openCloseBetslip())}
         className={`overlay ${!isWindowOpen}`}
       ></div>
+      <img className="footer" src={footer} />
       <BetslipPage />
     </React.Fragment>
   );
